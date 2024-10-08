b791d2a741ea70ac3d3cbb2dd8a4875a5fc0a690
ruan-cardozo <ruanvcardozo@gmail.com>
2024-10-07 22:08:07 -0300

    adjusting some security stuff

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const pool = require('./db');
 const authenticateToken = require('./auth');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 
 app.use(cors());
 app.use(express.json());
@@ -16,19 +17,24 @@ app.post('/api/login', async (req, res) => {
 	const { email, password } = req.body;
 
 	try {
-		const result = await pool.query(`SELECT * FROM users WHERE email = '${email}' and password = '${password}'`);
+		const result = await pool.query(`SELECT * FROM users WHERE email = $1  and password = $2`, [email, password]);
 		const user = result.rows[0];
 		
 		if (!result) {
 			return res.status(404).send('User not found');
 		}
 
-		if (!user || user.password !== password) {
-			return res.status(401).send('Invalid credentials');
+		const userIsValid = Boolean(bcrypt.compare(password, user.password));
+
+		console.log({userIsValid});
+
+		if (userIsValid) {
+			const accessToken = jwt.sign({ id: user.id, email: user.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+			res.json({ accessToken, userId: user.id });
 		}
 
-		const accessToken = jwt.sign({ id: user.id, email: user.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-		res.json({ accessToken, userId: user.id });
+		return res.status(401).send('Invalid credentials');
 	} catch (err) {
 		console.error(err);
 		res.status(500).send('Server Error');
@@ -77,17 +83,18 @@ app.get('/api/users/:id',authenticateToken, async (req, res) => {
 
 app.post('/api/users', async (req, res) => {
 	try {
-		const { name, email, password } = req.body;
+		let { name, email, password } = req.body;
 
-		const user = await pool.query(`SELECT * FROM users WHERE email = '${email}'`);
+		const user = await pool.query(`SELECT * FROM users WHERE email = $1`, [email]);
 		
 		if (user.email === email) {
 		
 			return res.status(400).send('User already exists');
 		}
 
-		const query = `INSERT INTO users (name, email, password) VALUES ('${name}', '${email}', '${password}') RETURNING *`;
-		const result = await pool.query(query);
+		password = encryptPassword(password);
+
+		const result = await pool.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [name, email, password]);
 
 		res.status(201).json(result.rows[0]);
 
@@ -128,3 +135,9 @@ app.delete('/api/users/:id', authenticateToken, async (req, res) => {
 		res.status(500).send('Server Error');
 	}
 });
+
+
+function encryptPassword(password) {
+
+	return bcrypt.hashSync(password, 10);
+}
