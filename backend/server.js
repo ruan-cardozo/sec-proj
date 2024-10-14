74bf6fb3cdb86502cb1cf076a8eeceba945ac8ba
ruan-cardozo <ruanvcardozo@gmail.com>
2024-10-14 16:36:09 -0300

    adding cors sec

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 
-app.use(cors());
+const corsOptions = {
+	origin: 'http://localhost:5173',
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.listen(3000, () => {
